5526c284c156f2045c754e6353b609d86d1b09c2
erika-hilty <erika.hity@gmail.com>
2020-04-04 20:56:15 -0400

    this is broke

--- db/index.js
+++ db/index.js
@@ -5,17 +5,76 @@ const { authenticate, compare, findUserFromToken, hash } = require('./auth');
 const models = ({ users } = require('./models'));
 
 const sync = async () => {
-  const SQL = `
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-    DROP TABLE IF EXISTS users;
-    CREATE TABLE users(
-      id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-      username VARCHAR(100) NOT NULL UNIQUE,
-      password VARCHAR(100) NOT NULL,
-      role VARCHAR(20) DEFAULT 'USER',
-      CHECK (char_length(username) > 0)
-    );
-  `;
+  let SQL = `
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  DROP TABLE IF EXISTS user_group CASCADE;
+  DROP TABLE IF EXISTS user_profile CASCADE;
+  DROP TABLE IF EXISTS user_hobbies CASCADE;  
+  DROP TABLE IF EXISTS meetup_locations CASCADE;
+  DROP TABLE IF EXISTS careers CASCADE;
+  DROP TABLE IF EXISTS user_rating CASCADE;
+  DROP TABLE IF EXISTS users CASCADE; 
+  CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(100) NOT NULL UNIQUE,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    password VARCHAR(100),
+    zipCode INT,
+    user_profile UUID,
+    user_group ,
+    userRating INT DEFAULT 0,
+    role VARCHAR(20) DEFAULT 'USER',
+    CHECK (char_length(username) > 0)
+  );
+  CREATE TABLE careers(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    career_name VARCHAR(100) NOT NULL
+  );
+  CREATE TABLE hobbies(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    hobby_name VARCHAR(20) NOT NULL
+  );
+  CREATE TABLE meetup_locations(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(100),
+    city VARCHAR(50),
+    state VARCHAR(20),
+    zip_code INT
+  );
+  
+  CREATE TABLE user_hobbies(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    hobby_id UUID REFERENCES hobbies(id),
+    user_id UUID REFERENCES users(id)
+  );
+  
+  CREATE TABLE user_rating(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID REFERENCES users(id),
+    rating INT
+  );
+  CREATE TABLE user_group(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID REFERENCES users(id),
+    name VARCHAR(100)
+  );
+  CREATE TABLE user_profile(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID REFERENCES users(id),
+    gender VARCHAR(100),
+    orientation VARCHAR(100),
+    politicalAffiliation VARCHAR(100),
+    religiousAffiliation VARCHAR(100),
+    careerId UUID REFERENCES careers(id),
+    education VARCHAR(100),
+    pets VARCHAR(100),
+    ageRange VARCHAR(100),
+    financialStatus VARCHAR(100)
+  );
+`;
+
   await client.query(SQL);
 
   const _users = {

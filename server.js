5cecc3ca138ec32ea93804aae7ae1bec3cdea8b2
morningzombie <terri@terribailey.com>
2020-04-05 19:31:36 -0400

    upload photo section

--- server.js
+++ server.js
@@ -1,30 +1,8 @@
 const express = require("express");
-//const app = express();
+const fileUpload = require("express-fileupload");
 const path = require("path");
 const db = require("./db");
-app = require("./app");
-
-app.use("/assets", express.static(path.join(__dirname, "assets")));
-const fileUpload = require("express-fileupload");
-
-app.use(fileUpload());
-
-//Upload endpoint
-app.post("/upload", (req, res) => {
-  if (req.files === null) {
-    return res.status(400).json({ msg: "No file uploaded" });
-  }
-
-  const file = req.files.file;
-
-  file.mv(`${_dirname}/client/public/uploads/${file.name}`, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
-    }
-    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
-  });
-});
+const app = require("./app");
 
 const port = process.env.PORT || 3090;
 
--- src/NewUserPhoto.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-import axios from "axios";
-
-const NewUserPhoto = () => {
-  return <div></div>;
-};
-export default NewUserPhoto;

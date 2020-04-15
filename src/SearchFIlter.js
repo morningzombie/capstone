0b9954c70c6a66491037c30fd595ddcb7ba628dc
erika-hilty <erika.hity@gmail.com>
2020-04-15 18:20:24 -0400

    asked to merge

--- src/SearchFIlter.js
+++ src/SearchFIlter.js
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const SearchFilter = ({ results, criteria }) => {
   const [filter, setFilter] = useState('');
